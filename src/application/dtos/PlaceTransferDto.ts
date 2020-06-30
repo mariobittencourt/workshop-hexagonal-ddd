c6a7aaed192bc9459c8dc6dcc131d65180c7abdf
Mario Bittencourt <Mario.Bittencourt@mindgeek.com>
2020-06-30 07:07:23 -0400

    Rename create to transfer to be aligned with ubiquitous language

--- src/application/dtos/PlaceTransferDto.ts
+++ src/application/dtos/PlaceTransferDto.ts
@@ -1,4 +1,4 @@
-export class CreateTransferDto {
+export class PlaceTransferDto {
     constructor(public readonly transferId: string, public readonly state: string) {
     }
 }
--- src/application/services/PlaceTransferHandler.ts
+++ src/application/services/PlaceTransferHandler.ts
@@ -4,7 +4,7 @@ import {Transfer, TransferStates} from "../../domain/models/Transfer";
 import {TransferId} from "../../domain/models/TransferId";
 import {TYPES} from "../../infrastructure/di/types";
 import {TransferRepository} from "../../domain/models/TransferRepository";
-import {CreateTransferDto} from "../dtos/CreateTransferDto";
+import {PlaceTransferDto} from "../dtos/PlaceTransferDto";
 import {Route} from "../../domain/models/Route";
 import {Location} from "../../domain/models/Location";
 
@@ -13,13 +13,13 @@ export class PlaceTransferHandler {
     constructor(@inject(TYPES.TransferRepository) private readonly repository: TransferRepository) {
     }
 
-    async handle(command: PlaceTransferCommand): Promise<CreateTransferDto> {
+    async handle(command: PlaceTransferCommand): Promise<PlaceTransferDto> {
         const origin = Location.createFromString(command.origin);
         const destination = Location.createFromString(command.destination);
         const transfer = Transfer.place(
             TransferId.create(),
             Route.create(origin, destination));
         await this.repository.add(transfer);
-        return new CreateTransferDto(transfer.id.toString(), TransferStates[transfer.state]);
+        return new PlaceTransferDto(transfer.id.toString(), TransferStates[transfer.state]);
     }
 }
